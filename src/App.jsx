d475472bab0c4b26266f0c315bcf3c0380f65d71
Kozimjon <kozimjonmalikov0727@gmail.com>
2024-09-09 17:14:44 +0500

    all files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Login from './layouts/Login'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Login />
+      </div>
+    )
+  }
+}
